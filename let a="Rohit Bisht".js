0d050e57d36e7c7cbf250fc8965c4995d80266e1
Rohit Bisht <147361720+Rohita827@users.noreply.github.com>
2024-01-21 05:35:10 +0000

    datatypes

---
+++ let a="Rohit Bisht".js
@@ -0,0 +1,2 @@
+let a="Rohit Bisht"
+console.log(a)
---
+++ re.js
@@ -0,0 +1,16 @@
+//let a="Rohit Bisht"
+var employeeid=123
+let employeeaddress="mnr"
+employeecity="ktm"
+let a="Raman"
+let b=5,c=4
+d=b+c
+//console.log("b"+c)
+//console.table([a,employeeaddress,employeeid,employeecity,d])
+
+const myfunction=function(){
+    console.log("Hello World");
+}
+e=null
+f=undefined
+console.log(typeof myfunction);
